ead1711297327e3aa8bff2ce1bb7472ac0f0fd6b
Diana Isabel Chávez <dianaichavez2@gmail.com>
2013-07-11 12:15:19 -0430

    help text models

--- activa24Duranjo/apps/productos/models.py
+++ activa24Duranjo/apps/productos/models.py
@@ -5,7 +5,7 @@ from tinymce.models import HTMLField
 
 class Categoria(models.Model):
 	titulo = models.CharField(max_length=200)
-	breve_descripcion = models.CharField(max_length=140) 
+	breve_descripcion = models.CharField(max_length=140, help_text='Hasta 140 caracteres') 
 	imagen = models.ImageField(upload_to='imgcategorias')
 	def __unicode__(self):
 		return self.titulo
@@ -13,7 +13,7 @@ class Categoria(models.Model):
 
 class Producto(models.Model):
 	titulo = models.CharField(max_length=200)
-	descripcion_corta = models.CharField(max_length=140)
+	descripcion_corta = models.CharField(max_length=140,help_text='Hasta 140 caracteres')
 	descripcion = HTMLField()
 	categoria = models.ForeignKey(Categoria)
 	precio_dolares = models.DecimalField(max_digits=30, decimal_places=3)
