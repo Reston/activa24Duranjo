dda87d16d1e4b8cdcf271972ca23b6902e0beb23
Diana Isabel Chávez <dianaichavez2@gmail.com>
2013-07-11 18:28:09 -0430

    url  pattern para categorias

--- activa24Duranjo/apps/productos/models.py
+++ activa24Duranjo/apps/productos/models.py
@@ -11,6 +11,11 @@ class Categoria(models.Model):
 	def __unicode__(self):
 		return self.titulo
 
+	@models.permalink
+	def get_absolute_url(self):
+		titulo = self.titulo.replace(' ', '-')
+		return ('categoria', {'titulo': titulo})
+
 
 class Producto(models.Model):
 	titulo = models.CharField(max_length=200)
--- activa24Duranjo/apps/productos/urls.py
+++ activa24Duranjo/apps/productos/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
 	'activa24Duranjo.apps.productos.views',
-	url(r'^categoria/$', 'categoria', name="productocategoria"),
+	url(r'^(?P<titulo>[-w]+)/$', 'categoria', name="productocategoria"),
 	url(r'^producto/$', 'producto', name="productoproducto"),
 )
