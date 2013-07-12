ab7e4845f543b5b21aa4a8d96049e30b8a2aaab2
Diana Isabel Chávez <dianaichavez2@gmail.com>
2013-07-12 14:44:28 -0430

    fix imagenes lista de productos

--- activa24Duranjo/apps/productos/views.py
+++ activa24Duranjo/apps/productos/views.py
@@ -16,6 +16,6 @@ def categoria(request, titulo):
 	lista = []
 	for p in productos:
 		lista.append(p.pk)
-	imagenes = ImgProductos.objects.filter(pk__in=lista)
+	imagenes = ImgProductos.objects.filter(producto__in=lista)
 	ctx = {'categoria':categoria, 'productos':productos, 'imagenes':imagenes}
 	return render_to_response('productos/categoria.html',ctx, context_instance=RequestContext(request))
