e8060c6b49cbb1705c5f1b420dfc104115b2fa23
José Ordoñez <restonjd@gmail.com>
2013-07-12 15:00:23 -0430

    fix categoria view

--- activa24Duranjo/apps/productos/views.py
+++ activa24Duranjo/apps/productos/views.py
@@ -13,9 +13,6 @@ def categoria(request, titulo):
 	titulo = titulo.replace('_', ' ')
 	categoria = Categoria.objects.get(titulo=titulo)
 	productos = Producto.objects.filter(categoria=categoria.pk)
-	lista = []
-	for p in productos:
-		lista.append(p.pk)
-	imagenes = ImgProductos.objects.filter(producto__in=lista)
-	ctx = {'categoria':categoria, 'productos':productos, 'imagenes':imagenes}
-	return render_to_response('productos/categoria.html',ctx, context_instance=RequestContext(request))
+	imagenes = ImgProductos.objects.filter(producto__in=productos)
+	ctx = {'categoria': categoria, 'productos': productos, 'imagenes': imagenes}
+	return render_to_response('productos/categoria.html', ctx, context_instance=RequestContext(request))
