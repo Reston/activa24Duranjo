12c81ce5153318c10a8acfea78807642cacebf01
Diana Isabel Chávez <dianaichavez2@gmail.com>
2013-07-14 14:41:41 -0430

    slider categoria

--- activa24Duranjo/apps/productos/views.py
+++ activa24Duranjo/apps/productos/views.py
@@ -12,10 +12,11 @@ def producto(request):
 def categoria(request, titulo):
 	titulo = titulo.replace('_', ' ')
 	categoria = Categoria.objects.get(titulo=titulo)
+	lista_categorias = Categoria.objects.all()
 	productos = Producto.objects.filter(categoria=categoria.pk)
 	lista = []
 	for p in productos:
 		lista.append(p.pk)
-	imagenes = ImgProductos.objects.filter(producto__in=lista)
-	ctx = {'categoria':categoria, 'productos':productos, 'imagenes':imagenes}
+	imagenes = ImgProductos.objects.filter(producto__in=productos)
+	ctx = {'categoria':categoria, 'productos':productos, 'imagenes':imagenes,'lista_categorias':lista_categorias}
 	return render_to_response('productos/categoria.html',ctx, context_instance=RequestContext(request))
