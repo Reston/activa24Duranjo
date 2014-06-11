bf94e9bcf5744110d2a9b151b94883595b73f5f1
JOSE ORDOÑEZ <restonjd@gmail.com>
2014-06-11 15:40:58 -0430

    si lo desean bannertexto

--- activa24Duranjo/apps/slider/models.py
+++ activa24Duranjo/apps/slider/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class SliderItem(models.Model):
 	title = models.CharField(max_length=140)
 	image = models.ImageField(upload_to='imgslider')
+	texto_banner = models.CharField(max_length=145)
 
 	def __unicode__(self):
 		return self.title
